2f5893f1a913e9a94829c433ff4b99d81eaf44b1
carm <carmelo.piccione@gmail.com>
2014-10-05 23:50:56 -0400

    robj.ml* compiles

--- src/lib/pb_response.mli
+++ src/lib/pb_response.mli
@@ -1,16 +1,15 @@
 open Core.Std
+module Key : Protobuf_capable.S
+module Value : Protobuf_capable.S
 
-module type Key = Protobuf_capable.S
-module type Value = Protobuf_capable.S
-
-type pair = (Key.t * string option)
-type keys = Key.t list
+type pair = (Key.t * string option) [@@deriving Protobuf]
+type keys = Key.t list [@@deriving Protobuf]
 
 type client_id   = string [@@deriving Protobuf]
 type server_info = (string option * string option) [@@deriving Protobuf]
 type list_buckets = string list  [@@deriving Protobuf]
 type list_keys    = (string list * bool)  [@@deriving Protobuf]
 type bucket_props = (Int32.t option * bool option) [@@deriving Protobuf]
-type get          = (Pb_robj.Content.t list * string option * bool option) [@@deriving Protobuf]
-type put          = (Pb_robj.Content.t list * string option * string option) [@@deriving Protobuf]
+type get          = (Robj.Content.t list * string option * bool option) [@@deriving Protobuf]
+type put          = (Robj.Content.t list * string option * string option) [@@deriving Protobuf]
 type index_search = (keys * pair list * string option * bool option) [@@deriving Protobuf]
--- src/lib/robj.mli
+++ src/lib/robj.mli
@@ -1,40 +1,40 @@
 open Core.Std
 
 
-module type Key = Protobuf_capable.S
-module type Value = Protobuf_capable.S
+module type Key = sig include Protobuf_capable.S end
+module type Value = sig include Protobuf_capable.S end
 
 module Link : functor(Key:Key) -> 
-sig include Protobuf_capable.S
+sig 
   type t = { bucket : string option [@key 1]
            ; key    : Key.t option [@key 2]
            ; tag    : string option [@key 3]
            } [@@deriving Protobuf]
+include Protobuf_capable.S with type t := t
 end
 
 module Pair : functor(Key:Key) (Value:Value) ->
 sig
-  include Protobuf_capable.S
   type t = { key: Key.t  [@key 1]
   ; value : Value.t option [@key 2]
   } [@@deriving Protobuf]
+  include Protobuf_capable.S with type t := t
 end
 
 
-module Content : sig
-  type t = { value            : string
-  ; content_type     : string option
-  ; charset          : string option
-  ; content_encoding : string option
-  ; vtag             : string option
-  ; links            : Link.t list
-  ; last_mod         : Int32.t option
-  ; last_mod_usec    : Int32.t option
-  ; usermeta         : Pair.t list
-  ; indices          : Pair.t list
-  ; deleted          : bool option
-  }
-
-  val parse : t Protobuf.Parser.t
-  val build : t -> (string, [> Protobuf.Builder.error ]) Result.t
+module Content : functor (Key:Key) (Value:Value) ->
+sig
+  type t = { value            : Value.t [@key 1]
+  ; content_type     : string option [@key 2]
+  ; charset          : string option [@key 3]
+  ; content_encoding : string option [@key 4]
+  ; vtag             : string option [@key 5]
+  ; links            : Link(Key).t list [@key 6]
+  ; last_mod         : Int32.t option [@key 7]
+  ; last_mod_usec    : Int32.t option [@key 8]
+  ; usermeta         : Pair(Key) (Value).t list [@key 9]
+  ; indices          : Pair(Key) (Value).t list [@key 10]
+  ; deleted          : bool option [@key 11]
+  } [@@deriving Protobuf]
+include Protobuf_capable.S with type t := t
 end
