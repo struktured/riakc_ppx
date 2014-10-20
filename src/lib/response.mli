52f2820fb9de9b1dfd1eacb46111415c9a976cf5
carm <carmelo.piccione@gmail.com>
2014-10-20 17:11:52 -0400

    linda is so cool

--- src/lib/response.mli
+++ src/lib/response.mli
@@ -5,22 +5,94 @@ module Result = Core.Std.Result
 
 type 'a t = More of 'a | Done of 'a
 
-
 type error = [ `Bad_payload | `Incomplete_payload | `Protobuf_encoder_error ]
+val error : string -> (unit t, [> error ]) Result.t
 
-val ping         : string -> (unit t, [> error ]) Result.t
+module type Ping = sig
+  type t = unit 
+  include Protobuf_capable.S with type t:=t
+end
 
-type client_id  = string [@@deriving Protobuf]
-type server_info = (string option * string option) [@@deriving Protobuf]
-type list_buckets = string list [@@deriving Protobuf]
-type bucket_props = (int option * bool option) [@@deriving Protobuf]
+val ping : string -> (Ping.t t, [> error ]) Result.t
+
+module type Client_id = sig
+  type t = string 
+  include Protobuf_capable.S with type t:=t
+end
+
+
+val client_id    : string -> (Client_id.t t, [> error ]) Result.t
+
+
+module type Client_id = sig
+  type t = string 
+  include Protobuf_capable.S with type t:=t
+end
+
+module type Server_info = sig
+  type t = (string option * string option) 
+  include Protobuf_capable.S with type t:=t
+end
+
+val server_info  : string -> (Server_info.t t, [> error ]) Result.t
+
+
+module type Buckets = sig 
+  type t = string list 
+  include Protobuf_capable.S with type t:=t
+end
+
+val list_buckets : string -> (Buckets.t t, [> error ]) Result.t
+
+module type Props = sig 
+  type t = (int option * bool option) 
+  include Protobuf_capable.S
+end
+
+val bucket_props : string -> (Props.t t, [> error ]) Result.t
 
 module Make : functor (Key:Key) (Value:Value) ->
 sig
 type pair = (Key.t * string option) [@@deriving Protobuf]
-type keys = Key.t list [@key 1] [@@deriving Protobuf]
-type list_keys    = (keys * bool)  [@@deriving Protobuf]
-type get          = (Robj.Content(Key) (Value).t list * string option * bool option) [@@deriving Protobuf]
-type put          = (Robj.Content(Key) (Value).t list * string option * string option) [@@deriving Protobuf]
-type index_search = (keys * pair list * string option * bool option) [@@deriving Protobuf]
+
+module type Keys = 
+  type t = Key.t list 
+  include Protobuf_capable.S with type t:=t
 end
+
+module type List_keys = sig 
+  type t = keys * bool
+  include Protobuf_capable.S with type t:=t 
+end
+
+val list_keys    : string -> (List_keys.t t, [> error ]) Result.t
+val delete       : string -> (unit t, [> error ]) Result.t
+
+module type Get = sig
+sig 
+  type t = Robj.Content(Key) (Value).t list * string option * bool option
+  include Protobuf_capable.S with type t:=t 
+end
+val get          : string -> ([ `Maybe_siblings ] Robj.t t, [> error ]) Result.t
+
+module type Put = sig 
+ type t = Robj.Content(Key) (Value).t list * string option * string option 
+ include Protobuf_capable.S with type t:=t 
+end
+val put          : string -> (([ `Maybe_siblings ] Robj.t * string option) t, [> error ]) Result.t
+
+
+module type Index_search = sig
+  type t = keys * pair list * string option * bool option 
+end
+
+val index_search :
+  string ->
+  (Index_search.t t, [> error ]) Result.t
+
+val index_search_stream :
+  string ->
+  (Index_search.t t, [> error ]) Result.t
+
+end
+
