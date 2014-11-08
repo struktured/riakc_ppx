b585cd1015feee7e2be8b9b5a981087877b53b57
carm <carmelo.piccione@gmail.com>
2014-11-08 16:44:12 -0500

    more string -> bytes

--- src/tests/all_tests.ml
+++ src/tests/all_tests.ml
@@ -17,17 +17,17 @@ module BytesCache = Caches.BytesCache
 
 module Rand = struct
   let lowercase = "abcdefghijklmnopqrstuvwxyz"
-  let alpha     = lowercase ^ String.uppercase lowercase
+  let alpha     = lowercase ^ Bytes.uppercase lowercase
   let num       = "0123456789"
   let alphanum  = alpha ^ num
   let symbols   = "!@#$%^&*();:<>,.?"
   let all       = alphanum ^ symbols
 
   let pick_n src len =
-    let s   = String.create len in
-    let s_l = String.length src in
+    let s   = Bytes.create len in
+    let s_l = Bytes.length src in
     for i = 0 to len - 1 do
-      String.set s i (String.get src (Random.int s_l))
+      Bytes.set s i (Bytes.get src (Random.int s_l))
     done;
     s
 
