72011f446a6e8d7a209d251eaf34149955694b94
paul <paul@guerriere>
2015-03-29 19:20:19 -0400

    Strange...conflict resolution appears to work...I thought it was broken.

--- src/tests/conflict_tests.ml
+++ src/tests/conflict_tests.ml
@@ -7,13 +7,15 @@ module Rconn = Riakc.Conn
 open Deferred.Result.Monad_infix
 
 module String = Cache.String
-module StringCache = Caches.StringCache
+(*module StringCache = Caches.StringCache*)
+module StringCache = Caches.StringPrimitiveCache;;
 module State = struct
   type t = unit
 
   let create () = ()
 end
-		       
+
+let testkey = "29March15B"
 (*Only need to be able to resolve any conflicts b/c right now cannot even do that.
   Conflict resolution here is meant to be done in client code, not by riak. The
   context includes use of a bucket type in which siblings are allowed, not the default
@@ -61,10 +63,12 @@ end = struct
     let rec helper alist resolved = 
       match alist with
       | [] -> resolved
-      | h::t -> let s = String.concat "" [resolved.a;h.a] in
+      | h::t -> let _ = printf "Considering next sibling with a:%s b:%d c:%s " resolved.a resolved.b (show_closedtype resolved.c) in
+		let s = String.concat "" [resolved.a;h.a] in
 		let updated = { resolved with a = s } in
 		let updated2 = { updated with b = (updated.b + h.b) } in
-	        { updated2 with c = Red } in
+	        let finalupdate = { updated2 with c = Red } in
+		helper t finalupdate in
     helper bunchofts init;;
 end 
 		       
@@ -88,7 +92,7 @@ let exec_update c updated_robj =
   let module R = StringCache.Robj in
   let open Opts.Put in 
   let opts = [Bucket_type "siblings_allowed"] in
-  StringCache.put c ~opts ~k:"testkey" updated_robj >>|
+  StringCache.put c ~opts ~k:testkey updated_robj >>|
     (fun x -> match x with
 		(robj, (Some key)) -> ()
 	      | (robj, None) -> ()
@@ -111,16 +115,18 @@ let get_and_resolve_conflicts c =
   let open Opts.Get in
   let open Response in
   let open Conflicted in
-  exec_get c "testkey" >>= function
-			 | robj -> (let rcrdtlist = rlist_from_content_list [] (R.contents robj) in
-				    let resolved = resolve_conflicts rcrdtlist in
-				    let e = Protobuf.Encoder.create () in
-				    let _ = recordt_to_protobuf resolved e in
-				    let pb = Protobuf.Encoder.to_string e in
-				    let old_content_hd = R.content robj in
-				    let new_content = R.Content.set_value pb old_content_hd in
-				    let updated_robj = R.set_content new_content robj in
-				    exec_update c updated_robj);;
+  exec_get c testkey >>= function
+		       | robj -> (let _ = printf "\nResolving conflicts amongst siblings..." in
+				  let rcrdtlist = rlist_from_content_list [] (R.contents robj) in
+				  let resolved = resolve_conflicts rcrdtlist in
+				  let _ = printf "Resolved with: field a:%s b:%d c:%s" resolved.a resolved.b (show_closedtype resolved.c) in				    
+				  let e = Protobuf.Encoder.create () in
+				  let _ = recordt_to_protobuf resolved e in
+				  let pb = Protobuf.Encoder.to_string e in
+				  let old_content_hd = R.content robj in
+				  let new_content = R.Content.set_value pb old_content_hd in
+				  let updated_robj = R.set_content new_content robj in
+				  exec_update c updated_robj);;
 (*
 			 | Core.Std.Result.Error `Notfound           -> printf "Not found"; ()
 			 | Core.Std.Result.Error `Bad_conn           -> printf "Bad_conn"; ()
@@ -132,7 +138,8 @@ let get_and_resolve_conflicts c =
 			 | Core.Std.Result.Error `Protobuf_encoder_error -> printf "Protobuf_encoder_error"; ()
 *)
   
-    
+(*This doesn't work. First put doesnt take for some reason despite no apparent errors 
+(Puts have lower level printfs on errors). Better to break this out...*)
 let resolve_test c =
   let open Opts.Put in
   let module Robj = StringCache.Robj in
@@ -142,38 +149,107 @@ let resolve_test c =
   let r2 = { a="second"; b=20; c=Red } in
   let r1aspb = string_of_pb_encoded_r r1 in
   let r2aspb = string_of_pb_encoded_r r2 in
-  let robj =
-    Robj.create
-      (Robj.Content.create r1aspb)
-  in
+  let robj = Robj.create (Robj.Content.create r1aspb) in
   let robj2 = Robj.create (Robj.Content.create r2aspb) in
-  let key = "testkey" in
+  let key = testkey in
   StringCache.put c ~opts ~k:key robj >>=
-    fun _ -> (Core.Std.Unix.sleep 6; Deferred.return(Ok ())) >>=
+    fun _ -> (let _ = printf "\nFirst put done..." in Core.Std.Unix.sleep 6; Deferred.return(Ok ())) >>=
     fun _ -> StringCache.put c ~opts ~k:key robj2 >>=
-    fun _ -> (Unix.sleep 6; Deferred.return(Ok ())) >>=
-    fun _ -> get_and_resolve_conflicts c >>=
-    fun _ -> exec_get c "testkey" >>=
+    fun _ -> (let _ = printf "\nSecond put done..." in Unix.sleep 6; Deferred.return(Ok ())) >>=
+    fun _ -> let _ = printf "\nGetting key with siblings..." in get_and_resolve_conflicts c >>=
+    fun _ -> exec_get c testkey >>=
     fun robj -> (let rlist = Robj.contents robj in
 		 if (List.length rlist > 1) then 
-		   (assert_cond "Failed to resolve conflict; siblings exist" (List.length rlist = 1))
+		   (assert_cond "\nFailed to resolve conflict; siblings exist" (List.length rlist = 1))
 		 else
 		   (let cont = Robj.content robj in
 		    let pb = Robj.Content.value cont in
 		    let r = recordt_of_pb pb in
 		    assert_cond
-		      "Value is wrong. Must have failed to resolve conflict or to put with vclock. Check db with curl."
+		      "\nValue is wrong. Must have failed to resolve conflict or to put with vclock. Check db with curl."
 		      ((r.b = 21) && (r.c=Red))))
 
-  
-let tests = [ ("resolve_test"           , resolve_test)]
+(*Do 2 puts*)
+let resolve_test_firstput c =
+  let open Opts.Put in
+  let module Robj = StringCache.Robj in
+  let open Conflicted in
+  let opts = [Bucket_type "siblings_allowed"] in 
+  let r1 = { a="first"; b=1; c=Green } in
+  let r1aspb = string_of_pb_encoded_r r1 in
+  let robj = Robj.create (Robj.Content.create r1aspb) in
+  let key = testkey in
+  StringCache.put c ~opts ~k:key robj >>=
+    fun _ -> (let _ = printf "\nFirst put done..." in Core.Std.Unix.sleep 6; Deferred.return(Ok ()))
+
+let resolve_test_secondput c =
+  let open Opts.Put in
+  let module Robj = StringCache.Robj in
+  let open Conflicted in
+  let opts = [Bucket_type "siblings_allowed"] in 
+  let r2 = { a="second"; b=20; c=Red } in
+  let r2aspb = string_of_pb_encoded_r r2 in
+  let robj2 = Robj.create (Robj.Content.create r2aspb) in
+  let key = testkey in
+  StringCache.put c ~opts ~k:key robj2 >>=
+    fun _ -> (let _ = printf "\nSecond put done..." in Core.Std.Unix.sleep 6; Deferred.return(Ok ()))
+
+let get_resolve_put_test c =
+  let module R = StringCache.Robj in
+  let module C = StringCache.Robj.Content in
+  let open Opts.Get in
+  let open Response in
+  let open Conflicted in
+  let option_to_string_vclock = Option.value ~default:"" in
+  exec_get c testkey >>= function
+		       | robj -> (let _ = printf "\nResolving conflicts amongst siblings..." in
+				  let rcrdtlist = rlist_from_content_list [] (R.contents robj) in
+				  let thevclock = (R.vclock robj) in		  
+				  let _ = printf "\nFound %d siblings and robj vclock:%s " (List.length rcrdtlist) (option_to_string_vclock thevclock) in
+				  let resolved = resolve_conflicts rcrdtlist in
+				  let _ = printf "Resolved with: field a:%s b:%d c:%s" resolved.a resolved.b (show_closedtype resolved.c) in				    
+				  let e = Protobuf.Encoder.create () in
+				  let _ = recordt_to_protobuf resolved e in
+				  let pb = Protobuf.Encoder.to_string e in
+				  let old_content_hd = R.content robj in
+				  let new_content = R.Content.set_value pb old_content_hd in
+				  let updated_robj = R.set_content new_content robj in
+				  exec_update c updated_robj) >>=
+				   fun _ -> exec_get c testkey >>=
+				   fun robj -> (let rlist = R.contents robj in
+						if (List.length rlist > 1) then 
+						  (assert_cond "\nFailed to resolve conflict; siblings exist" (List.length rlist = 1))
+						else
+						  (let cont = R.content robj in
+						   let pb = R.Content.value cont in
+						   let r = recordt_of_pb pb in
+						   assert_cond
+						     "\nValue is wrong. Must have failed to resolve conflict or to put with vclock. Check db with curl."
+						     ((r.b = 21) && (r.c=Red))))
+	       
+(*==========BOTH OF THESE APPROACHES FAIL TO PUT TWICE TO SAME KEY AND GET A SIBLING
+Not sure why...is riak that slow? Why no apparent error?==============*)
+(*let tests = [ ("resolve_test"           , resolve_test)]*)
+(*let tests = [ ("resolve_test_firstput"           , resolve_test_firstput);
+	      ("resolve_test_secondput"           , resolve_test_secondput);
+	      ("get_resolve_put_test", get_resolve_put_test)]*)
+(*==================*)
+(*Back to old way naiive: force tester to recompile and re-execute 3 times, each time changing which "test" runs, first
+one makes first put, second one creates sibling with second put. Third and final real test gets and tries to resolve 
+sibling conflict then make a put. And WHILE we're troubleshooting we only needs to invoke the first 2 puts ONCE. We'll
+be focusing on fixing the 3rd step that is failing: the get, resolve, put cycle that is supposed to use a vclock to 
+resolve conflicts. Once you do the first 2 puts just keep working with those siblings.*)
+(*===========FIRST PUT======= let tests = [ ("resolve_test_firstput", resolve_test_firstput)] *)
+(*===========SECOND PUT creates sibling======= let tests = [ ("resolve_test_secondput", resolve_test_secondput)] *)
+(*===========CONFLICT RESOLUTION: resolution of conflict is faiing=======*)
+let tests = [ ("get_resolve_put_test", get_resolve_put_test)]
 	      
 let execute_test t =
   let with_cache () =
     StringCache.with_cache
       ~host:"127.0.0.1"
       ~port:8087
-      ~bucket:(Sys.argv.(3))
+      ~bucket:"with_sibs"
       (fun cache -> t cache)
   in
   with_cache ()
